a73805fb42bcf77964281c3a8cee8678640a8cca
hf928 <haofu.hsieh@gmail.com>
2018-12-23 22:51:32 +0800

    add html5example - webworkers

---
+++ html5example/webworkers/js/workers.js
@@ -0,0 +1,27 @@
+﻿let max = 1e8,
+    temp = 0,
+    times = 0,
+    i;
+
+postMessage('1億次累加運算開始 (1+2+3+4+...+1億)');
+postMessage('-----------------------------------');
+
+while (max) {
+
+    times = times + 1;
+    temp = temp + times;
+
+    i = 1;
+
+    for (i; i <= 10; i++) {
+
+        if (i * 1e7 === times) postMessage(`完成: ${i}0%，目前累計: ${temp}`);
+
+    };
+
+    max = max - 1;
+
+};
+
+postMessage('-----------------------------------');
+postMessage('運算結束');
