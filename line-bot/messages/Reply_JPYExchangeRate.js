c8326f57589168a4e3db5efb2705b5ffff4bf829
hf928 <haofu.hsieh@gmail.com>
2019-10-13 18:56:39 +0800

    Update Reply_JPYExchangeRate.js
    修改寫法

--- line-bot/messages/Reply_JPYExchangeRate.js
+++ line-bot/messages/Reply_JPYExchangeRate.js
@@ -1,14 +1,20 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+// 台銀匯率頁面 url
+const rateUrl = 'https://rate.bot.com.tw/xrt?Lang=zh-TW';
 
-let timer;
-const delay = 30 * 60 * 1000;
-let jpyExRate = 0;
+// 每半小時重置暫存匯率
+const t = 30 * 60 * 1000;
 
-const checkJapanExRate = () => {
+// 暫存匯率
+let jpyExRateTemp = 0;
 
-    clearTimeout(timer);
+// 重置暫存匯率
+setInterval(() => jpyExRateTemp = 0, t);
+
+// 爬台灣銀行匯率
+const getJapanExRate = () => new Promise((resolve, reject) => {
 
     request({
         url: 'http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm',
@@ -18,43 +24,58 @@ const checkJapanExRate = () => {
         if (body && !error) {
 
             const querySelector = cheerio.load(body);
-            const target = querySelector('.rate-content-sight.text-right.print_hide');
-            jpyExRate = target[15].children[0].data;
+            // 日本現金賣出匯率 element
+            const target = querySelector('.rate-content-cash.text-right.print_hide')[15];
+            const jpyExRate = +target.children[0].data;
 
-            timer = setTimeout(checkJapanExRate, delay);
+            resolve(jpyExRate);
             
+        }
+        else {
+
+            console.log(error);
+            reject(error);
+
         }
 
     });
 
-}
-
-checkJapanExRate();
+});
   
 module.exports = (bot) => {
 
-    bot.on('message', (evt) => {
+    bot.on('message', async (evt) => {
 
+        // 收到文字訊息
         if (evt.message.type === 'text') {
 
             const msg = evt.message.text;
     
+            // 文字包含關鍵字: '匯率'
             if (msg.indexOf('匯率') !== -1) {
 
-                let replyMsg = '機器人跑跑跑, 請喝口水再詢問';
-    
-                if (jpyExRate > 0) {
+                let replyMsg = '';
 
-                    replyMsg = `日幣匯率 ${jpyExRate} 啦 (溫柔)
-https://rate.bot.com.tw/xrt?Lang=zh-TW`;
+                // 檢查是否有暫存匯率, 沒有就去爬
+                if (jpyExRateTemp === 0) {
+
+                    // 爬匯率
+                    const jpyExRate = await getJapanExRate();
+
+                    // 暫存匯率以供下次使用
+                    jpyExRateTemp = jpyExRate;
 
                 }
 
+                
+                replyMsg = `日幣匯率 ${jpyExRateTemp}
+${rateUrl}`;
+
+                // 回應訊息
                 evt.reply(replyMsg);
 
             }
             
-    
         }
     
     });
