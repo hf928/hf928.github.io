175ff0969c14bf3a7942baa164f3f8343619b967
hf928 <haofu.hsieh@gmail.com>
2020-04-24 01:54:27 +0800

    Updates

---
+++ precache-manifest.1538c0870d351f84182d5dad6f157242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40918a8507c7938d25f223811cd15dc",
+    "url": "/minna-calculator/index.html"
+  },
+  {
+    "revision": "a6ca24d8d99a7d5049bf",
+    "url": "/minna-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25daa53754122bfcfc3c",
+    "url": "/minna-calculator/static/js/2.2eb3068b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/minna-calculator/static/js/2.2eb3068b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ca24d8d99a7d5049bf",
+    "url": "/minna-calculator/static/js/main.faa0111a.chunk.js"
+  },
+  {
+    "revision": "988055a9eace1e2ecd55",
+    "url": "/minna-calculator/static/js/runtime-main.d9044675.js"
+  }
+]);
